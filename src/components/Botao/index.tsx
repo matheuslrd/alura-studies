a75d85f3dc674136c7a1c81a2f5a1ab82217f140
Matheus Laurindo <matheus104521@gmail.com>
2023-03-21 11:09:16 -0300

    (Botao) refactor: Class Component -> Function

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import style from './Botao.module.scss';
 
-class Botao extends React.Component<{
+type Props = {
   type?: 'button' | 'submit' | 'reset' | undefined;
   onClick?: () => void;
-}> {
-  render() {
-    const { type = 'button', onClick } = this.props;
-    return (
-      <button type={type} className={style.botao} onClick={onClick}>
-        {this.props.children}
-      </button>
-    );
-  }
-}
+};
 
-export default Botao;
+export default function Botao(props: PropsWithChildren<Props>) {
+  const { onClick, type } = props;
+
+  return (
+    <button type={type} className={style.botao} onClick={onClick}>
+      {props.children}
+    </button>
+  );
+}
